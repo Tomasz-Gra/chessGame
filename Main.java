f06a00b35379095bdbd86a73a9177aa032a81f35
TomaszZayl <42319689+TomaszZayl@users.noreply.github.com>
2018-08-24 09:38:43 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package chessGame;
+
+class Main {
+
+    public static void main(String[] args) {
+       DrawNewFrame drawNewFrame = new DrawNewFrame();
+       drawNewFrame.drawFrameGUI();
+    }
+
+}
