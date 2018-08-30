f3c5b48ae542469b3b6fc61402fca7407d482cdf
TomaszZayl <42319689+TomaszZayl@users.noreply.github.com>
2018-08-30 15:50:26 +0200

    Update MovePieces.java

--- chess/MovePieces.java
+++ chess/MovePieces.java
@@ -3,12 +3,13 @@ package chessGame;
 import javax.swing.*;
 
 class MovePieces extends SetValuesForControllers {
+    private boolean NE() { return secondClicked() == buttons[fieldZero() - 1][fieldOne() + 1]; }
+    private boolean NW() { return secondClicked() == buttons[fieldZero() - 1][fieldOne() - 1]; }
+    private boolean SE() { return secondClicked() == buttons[fieldZero() + 1][fieldOne() + 1]; }
+    private boolean SW() { return secondClicked() == buttons[fieldZero() + 1][fieldOne() - 1]; }
 
     void moveBlackPawn() {
-        boolean SE = ((secondClicked() == buttons[fieldZero() + 1][fieldOne() + 1]) && !iconIsNull());
-        boolean SW = ((secondClicked() == buttons[fieldZero() + 1][fieldOne() - 1]) && !iconIsNull());
-
-        if (SE || SW) {
+        if (SE() && !iconIsNull() || SW()&& !iconIsNull()) {
             doMove();
         } else if ((secondClicked() != buttons[fieldZero() + 1][fieldOne()]) || !iconIsNull()) {
             clearTextfields();
@@ -18,10 +19,7 @@ class MovePieces extends SetValuesForControllers {
     }
 
     void moveWhitePawn() {
-        boolean NW = ((secondClicked() == buttons[fieldZero() - 1][fieldOne() - 1]) && !iconIsNull());
-        boolean NE = ((secondClicked() == buttons[fieldZero() - 1][fieldOne() + 1]) && !iconIsNull());
-
-        if (NW || NE) {
+        if (NW() && !iconIsNull() || NE() && !iconIsNull()) {
             doMove();
         } else if ((secondClicked() != buttons[fieldZero() - 1][fieldOne()]) || !iconIsNull()) {
             clearTextfields();
@@ -37,13 +35,29 @@ class MovePieces extends SetValuesForControllers {
 
         JButton[] checkIfCanMove = {buttons[fieldZero() + 2][fieldOne() + 1], buttons[fieldZero() + 2][fieldOne() - 1], buttons[fieldZero() - 2][fieldOne() + 1], buttons[fieldZero() - 2][fieldOne() - 1], buttons[fieldZero() + 1][fieldOne() + 2], buttons[fieldZero() + 1][fieldOne() - 2], buttons[fieldZero() - 1][fieldOne() + 2], buttons[fieldZero() - 1][fieldOne() - 2]};
 
-        for (int checkMovement = 0; checkMovement < checkIfCanMove.length; checkMovement++) { booleans[checkMovement] = secondClicked() == checkIfCanMove[checkMovement]; }
-        for (int iconIsNull = 0; iconIsNull < booleans.length; iconIsNull++) { bIconIsNull[iconIsNull] = booleans[iconIsNull] && iconIsNull(); }
-        for (int iconIsNotNull = 0; iconIsNotNull < booleans.length; iconIsNotNull++) { bIconIsNotNull[iconIsNotNull] = booleans[iconIsNotNull] && !iconIsNull(); }
+        for (int checkMovement = 0; checkMovement < checkIfCanMove.length; checkMovement++) {
+            booleans[checkMovement] = secondClicked() == checkIfCanMove[checkMovement];
+        }
+        for (int iconIsNull = 0; iconIsNull < booleans.length; iconIsNull++) {
+            bIconIsNull[iconIsNull] = booleans[iconIsNull] && iconIsNull();
+        }
+        for (int iconIsNotNull = 0; iconIsNotNull < booleans.length; iconIsNotNull++) {
+            bIconIsNotNull[iconIsNotNull] = booleans[iconIsNotNull] && !iconIsNull();
+        }
 
         if (bIconIsNull[0] || bIconIsNull[1] || bIconIsNull[2] || bIconIsNull[3] || bIconIsNull[4] || bIconIsNull[5] || bIconIsNull[6] || bIconIsNull[7]) {
             doMove();
         } else if (bIconIsNotNull[0] || bIconIsNotNull[1] || bIconIsNotNull[2] || bIconIsNotNull[3] || bIconIsNotNull[4] || bIconIsNotNull[5] || bIconIsNotNull[6] || bIconIsNotNull[7]) {
+            doMove();
+        } else {
+            clearTextfields();
+        }
+    }
+
+    void moveBishop() {
+        if (NE() && !iconIsNull() || NW() && !iconIsNull() || SE() && !iconIsNull() || SW() && !iconIsNull()) {
+            doMove();
+        } else if (NE() && iconIsNull() || NW() && iconIsNull() || SE() && iconIsNull() || SW() && iconIsNull()) {
             doMove();
         } else {
             clearTextfields();
