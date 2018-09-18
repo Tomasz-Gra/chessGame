11880b731aea29309809197d5a8d3a20f45a7555
TomaszZayl <42319689+TomaszZayl@users.noreply.github.com>
2018-09-18 10:19:15 +0200

    Update MovePieces.java

--- chess/MovePieces.java
+++ chess/MovePieces.java
@@ -31,9 +31,15 @@ class MovePieces extends SetValuesForControllers{
 
         JButton[] checkIfCanMove = {buttons[fieldZero() + 2][fieldOne() + 1], buttons[fieldZero() + 2][fieldOne() - 1], buttons[fieldZero() - 2][fieldOne() + 1], buttons[fieldZero() - 2][fieldOne() - 1], buttons[fieldZero() + 1][fieldOne() + 2], buttons[fieldZero() + 1][fieldOne() - 2], buttons[fieldZero() - 1][fieldOne() + 2], buttons[fieldZero() - 1][fieldOne() - 2]};
 
-        for (int checkMovement = 0; checkMovement < checkIfCanMove.length; checkMovement++) { booleans[checkMovement] = secondClicked() == checkIfCanMove[checkMovement]; }
-        for (int iconIsNull = 0; iconIsNull < booleans.length; iconIsNull++) { bIconIsNull[iconIsNull] = booleans[iconIsNull] && iconIsNull(); }
-        for (int iconIsNotNull = 0; iconIsNotNull < booleans.length; iconIsNotNull++) { bIconIsNotNull[iconIsNotNull] = booleans[iconIsNotNull] && !iconIsNull(); }
+        for (int checkMovement = 0; checkMovement < checkIfCanMove.length; checkMovement++) {
+            booleans[checkMovement] = secondClicked() == checkIfCanMove[checkMovement];
+        }
+        for (int iconIsNull = 0; iconIsNull < booleans.length; iconIsNull++) {
+            bIconIsNull[iconIsNull] = booleans[iconIsNull] && iconIsNull();
+        }
+        for (int iconIsNotNull = 0; iconIsNotNull < booleans.length; iconIsNotNull++) {
+            bIconIsNotNull[iconIsNotNull] = booleans[iconIsNotNull] && !iconIsNull();
+        }
 
         if (bIconIsNull[0] || bIconIsNull[1] || bIconIsNull[2] || bIconIsNull[3] || bIconIsNull[4] || bIconIsNull[5] || bIconIsNull[6] || bIconIsNull[7]) {
             doMove();
@@ -70,6 +76,16 @@ class MovePieces extends SetValuesForControllers{
         } else if (fightOneUp() || (fightTwoUp() && isIconOneUp() == null)  || fightOneDown() || (fightTwoDown() && isIconOneDown() == null) || fightOneRight() || (fightTwoRight() && isIconOneRight() == null) || fightOneLeft() || (fightTwoLeft() && isIconOneLeft() == null) ) {
             doMove();
         } else if (fightOneDownOneRight() || (fightTwoDownTwoRight() && isIconOneDownOneRight() == null) || fightOneDownOneLeft() || (fightTwoDownTwoLeft() && isIconOneDownOneLeft() == null) || fightOneUpOneRight() || (fightTwoUpTwoRight() && isIconOneUpOneRight() == null) || fightOneUpOneLeft() || (fightTwoUpTwoLeft() && isIconOneUpOneLeft() == null)) {
+            doMove();
+        } else {
+            clearTextfields();
+        }
+    }
+
+    void moveKing() {
+        if ((fightOneUp()) || fightOneDown() || fightOneRight() || fightOneLeft() || fightOneUpOneRight() || fightOneUpOneLeft() || fightOneDownOneRight() || fightOneDownOneLeft()) {
+            doMove();
+        } else if ((moveOneUp()) || moveOneDown() || moveOneRight() || moveOneLeft() || moveOneUpOneRight() || moveOneUpOneLeft() || moveOneDownOneRight() || moveOneDownOneLeft()) {
             doMove();
         } else {
             clearTextfields();
